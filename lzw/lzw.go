4ce4d5005555ed29fac0618785049eb373829dbc
Emmanuel Goossaert <emmanuel.goossaert@gmail.com>
2019-01-15 08:22:06 +0100

    Fixed bug in encoder.

--- lzw/lzw.go
+++ lzw/lzw.go
@@ -1,10 +1,11 @@
 package lzw
 
 import (
-    "fmt"
     "io"
     "strings"
     "encoding/binary"
+
+    "github.com/goossaert/compression/logging"
 )
 
 // TODO need more idiomatic use of byte slides and strings
@@ -15,7 +16,7 @@ import (
 
 func Compress(rawData io.Reader) (compressedData *[]byte, nbits int) {
     out := make([]byte, 0)
-    fmt.Printf("Compress()")
+    logging.Trace.Printf("Compress()\n")
     stringsToCodes := make(map[string]uint16)
     for i := 0 ; i < 256 ; i++ {
         stringsToCodes[string(i)] = uint16(i)
@@ -31,22 +32,26 @@ func Compress(rawData io.Reader) (compressedData *[]byte, nbits int) {
         n, err := rawData.Read(readBuffer)
         if err == io.EOF {
             // Flush out the last string to encode
-            outputCode := stringsToCodes[string(window[:sizeWindow-1])]
+            outputCode := stringsToCodes[string(window[:sizeWindow])]
+            logging.Trace.Printf("ENC %s => %d\n", string(window[:sizeWindow]), outputCode)
             binary.LittleEndian.PutUint16(outputBytes, uint16(outputCode))
             out = append(out, outputBytes[0], outputBytes[1])
             nbits += 16
             break
         }
+        logging.Trace.Printf("nbytes read: %d\n", n)
         for i := 0 ; i < n ; i++ {
             window = append(window, readBuffer[i])
             sizeWindow += 1
-            if _, exists := stringsToCodes[string(window[:sizeWindow])]; exists == true {
+            if _, ok := stringsToCodes[string(window[:sizeWindow])]; ok {
                 continue
             }
 
             stringsToCodes[string(window[:sizeWindow])] = nextCode
+            logging.Trace.Printf("ADD %s => %d\n", string(window[:sizeWindow]), nextCode)
             nextCode += 1
             outputCode := stringsToCodes[string(window[:sizeWindow-1])]
+            logging.Trace.Printf("ENC %s => %d\n", string(window[:sizeWindow-1]), outputCode)
             binary.LittleEndian.PutUint16(outputBytes, uint16(outputCode))
             out = append(out, outputBytes[0], outputBytes[1])
             nbits += 16
@@ -63,7 +68,7 @@ func Compress(rawData io.Reader) (compressedData *[]byte, nbits int) {
 
 func Uncompress(compressedData io.Reader, nbits int) (uncompressedData *[]byte) {
     out := make([]byte, 0)
-    fmt.Printf("Uncompress()")
+    logging.Trace.Printf("Uncompress()\n")
     codesToStrings := make(map[uint16]string)
     for i := 0 ; i < 256 ; i++ {
         codesToStrings[uint16(i)] = string(i)
@@ -81,18 +86,21 @@ func Uncompress(compressedData io.Reader, nbits int) (uncompressedData *[]byte) 
         }
         for i := 0 ; i < n ; i += 2 {
             var code uint16 = binary.LittleEndian.Uint16(readBuffer[i:i+2])
-            if _, exists := codesToStrings[code] ; exists == false {
+            if _, ok := codesToStrings[code] ; !ok {
                 stringBuilder.Reset()
                 stringBuilder.WriteString(*previousString)
                 stringBuilder.WriteByte((*previousString)[0])
                 codesToStrings[code] = stringBuilder.String()
+                logging.Trace.Printf("ADDN %s => %d\n", stringBuilder.String(), code)
             }
             out = append(out, codesToStrings[code]...)
+            logging.Trace.Printf("DEC %d => %s\n", code, codesToStrings[code])
             if previousString != nil && len(*previousString) > 0 {
                 stringBuilder.Reset()
                 stringBuilder.WriteString(*previousString)
                 stringBuilder.WriteByte(codesToStrings[code][0])
                 codesToStrings[nextCode] = stringBuilder.String()
+                logging.Trace.Printf("ADD %s => %d\n", stringBuilder.String(), nextCode)
                 nextCode += 1
             }
             newString := codesToStrings[code]
